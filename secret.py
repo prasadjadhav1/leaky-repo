f450f8780cc89ddbcedb506f17e173faab3a2ea9
prasad <prasad.dattatrayjadhav@hashicorp.com>
2025-06-04 10:00:23 +0530

    added new token

--- secret.py
+++ secret.py
@@ -1,4 +1,6 @@
 # GitHub Personal Access token
-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
+'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'
 
 token = 'nAH2VzKrMrRjySLlt8HCdFU3tM2TUuUZgh39NX'
+
+'ghp_rfADHPiJeKRhiz812QHQomgoXhsV7t17kcCE'
